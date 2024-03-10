788c89c03d8f01b1f6dee477b0b27c9046b2dae3
tonolipoli <andrea2.tonoli@mail.polimi.it>
2024-03-10 20:32:26 +0100

    cambiamenti nel costruttore della classe Player

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -3,25 +3,24 @@ package model;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 public class Player {
     private final String username;
     private boolean firstToEnd;
     private boolean firstToPlay;
     private Card[] cardInHand = new Card[3];
-    private ObjectiveCard[] personalObj = new ObjectiveCard[2];
+    private AchievementCard[] personalObj = new AchievementCard[2];
     private final StarterCard starterCard;
     private int points;
     private PlayerState playerState;
     private int[] SymbolCount = {0,0,0,0};
 
 
-    public Player(String name,StarterCard sCard ,ObjectiveCard obj1, ObjectiveCard obj2, ResourceCard rCard1, ResourceCard rCard2, GoldCard gCard){
+    public Player(String name,StarterCard sCard ,AchievementCard[] obj, GoldCard gCard,ResourceCard[] hand){
         this.username = name;
-        cardInHand[0] = rCard1;
-        cardInHand[1] = rCard2;
-        cardInHand[2] = gCard;
-        personalObj[0] = obj1;
-        personalObj[1] = obj2;
         this.starterCard = sCard;
+        System.arraycopy(obj, 0, personalObj, 0, 2);
+        System.arraycopy(hand, 0, cardInHand, 0, 2);
+        cardInHand[2] = gCard;
     }
 }
