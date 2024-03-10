c569738c350076f7b9cf20bcf441a5dc8a14d4d6
Francesca <francescadora.pieruz@mail.polimi.it>
2024-03-10 12:42:04 +0100

    Add methods and attributes to class game

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,4 +1,35 @@
 package model;
 
 public class Game {
+    private int minPlayer;
+    private int maxPlayer;
+    private List<Player> players;
+
+    public void startGame()
+    {
+
+    }
+
+    public void endGame()
+    {
+
+    }
+
+    public void setFirstPlayer()
+    {
+
+    }
+
+    public void setGameState()
+    {
+
+    }
+
+    public void setPlayerInTurn()
+    {
+
+    }
+
+
+
 }
