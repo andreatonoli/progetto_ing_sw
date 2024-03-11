f72e81dc003f06218a56287914e494e3aae52493
Stefano <stefano23.riva@mail.polimi.it>
2024-03-11 22:58:36 +0100

    Added Card.getCorner function in Card.java

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -1,10 +1,15 @@
 package model;
 
-import java.util.List;
-
 public abstract class Card implements ICard {
     protected Corner[] corners;
     /** Card's ID is composed of its type and its card_number value */
     protected String type;
     protected int card_number;
+    protected Card currentSide;
+    public Corner[] getCorners() {
+        return corners;
+    }
+    public Corner getCorner(int index){
+        return corners[index];
+    }
 }
