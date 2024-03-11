45d03fc3a408152a4dc19066987333e3beeac06a
tonolipoli <andrea2.tonoli@mail.polimi.it>
2024-03-11 15:56:10 +0100

    Definition of some methods in the Player class.

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -11,16 +11,38 @@ public class Player {
     private Card[] cardInHand = new Card[3];
     private AchievementCard[] personalObj = new AchievementCard[2];
     private final StarterCard starterCard;
-    private int points;
+    private int points = 0;
     private PlayerState playerState;
-    private int[] SymbolCount = {0,0,0,0};
+    //
+    private int[] SymbolCount = {0,0,0,0,0,0,0};
 
-
+    /**
+     * constructor of the player class:
+     * @param name is the player's unique username
+     * @param sCard is the player's starting card
+     * @param obj array of 2 achievement card, contains the player's personal objectives
+     * @param gCard is the player's initial golden card
+     * @param hand array of 2 resource card, contains the player's initial resource card
+     */
     public Player(String name,StarterCard sCard ,AchievementCard[] obj, GoldCard gCard,ResourceCard[] hand){
         this.username = name;
         this.starterCard = sCard;
         System.arraycopy(obj, 0, personalObj, 0, 2);
         System.arraycopy(hand, 0, cardInHand, 0, 2);
         cardInHand[2] = gCard;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public int getPoints(){
+        return points;
+    }
+
+    public int addPoints(int pointToAdd){
+        this.points = this.points + pointToAdd;
+        if (this.points > 29)
+            this.points = 29;
     }
 }
