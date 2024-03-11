df789ad52986e4adbeedcc1f4dee170658282293
tonolipoli <andrea2.tonoli@mail.polimi.it>
2024-03-11 19:34:22 +0100

    Definition of some more methods in the Player class, first beta of the method addSymbolCount.

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,9 +1,5 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Player {
     private final String username;
     private boolean firstToEnd;
@@ -32,17 +28,90 @@ public class Player {
         cardInHand[2] = gCard;
     }
 
+    /**
+     *
+     * @return player's username
+     */
     public String getUsername(){
         return username;
     }
 
+    /**
+     * setter to change the player state
+     * @param playerState is the new state of the player
+     */
+    public void setPlayerState(PlayerState playerState) {
+        this.playerState = playerState;
+    }
+
+    /**
+     * getter to get the current player state
+     * @return the current player state
+     */
+    public PlayerState getPlayerState() {
+        return playerState;
+    }
+
+    /**
+     * getter to get the current score of the player
+     * @return the player's points
+     */
     public int getPoints(){
         return points;
     }
 
-    public int addPoints(int pointToAdd){
-        this.points = this.points + pointToAdd;
+    public int addPoints(int pointsToAdd){
+        this.points = this.points + pointsToAdd;
         if (this.points > 29)
             this.points = 29;
+        return this.points;
+    }
+
+    public void isFirstToPlay(String username){
+        firstToPlay = this.username.equals(username);
+    }
+
+    public void isFirstToEnd(String username){
+        firstToEnd = this.username.equals(username);
+    }
+
+    //manca da inserire l'aumento di punteggio per i centri delle backcard
+    public void addSymbolCount(Card placedCard, Corner coveredCorner){
+        int[] corner = placedCard.getCorner();
+
+        for (int i=0; i<4; i++)
+            if (corner[i].getSymbol() == 'PLANT')
+                SymbolCount[0]=SymbolCount[0]+1;
+            else if (corner[i].getSymbol() == 'ANIMAL')
+                SymbolCount[1]=SymbolCount[1]+1;
+            else if (corner[i].getSymbol() == 'FUNGI')
+                SymbolCount[2]=SymbolCount[2]+1;
+            else if (corner[i].getSymbol() == 'INSECT')
+                SymbolCount[3]=SymbolCount[3]+1;
+            else if (corner[i].getSymbol() == 'QUILL')
+                SymbolCount[4]=SymbolCount[4]+1;
+            else if (corner[i].getSymbol() == 'INKWELL')
+                SymbolCount[5]=SymbolCount[5]+1;
+            else if (corner[i].getSymbol() == 'MANUSCRIPT')
+                SymbolCount[6]=SymbolCount[6]+1;
+
+        if (coveredCorner.getSymbol() == 'PLANT')
+            SymbolCount[0]=SymbolCount[0]-1;
+        else if (coveredCorner.getSymbol() == 'ANIMAL')
+            SymbolCount[1]=SymbolCount[1]-1;
+        else if (coveredCorner.getSymbol() == 'FUNGI')
+            SymbolCount[2]=SymbolCount[2]-1;
+        else if (coveredCorner.getSymbol() == 'INSECT')
+            SymbolCount[3]=SymbolCount[3]-1;
+        else if (coveredCorner.getSymbol() == 'QUILL')
+            SymbolCount[4]=SymbolCount[4]-1;
+        else if (coveredCorner.getSymbol() == 'INKWELL')
+            SymbolCount[5]=SymbolCount[5]-1;
+        else if (coveredCorner.getSymbol() == 'MANUSCRIPT')
+            SymbolCount[6]=SymbolCount[6]-1;
+    }
+
+    public int[] getSymbolCount(){
+        return SymbolCount;
     }
 }
