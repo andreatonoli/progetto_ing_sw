3c058e2bdb75bcf0eb5ea86a868dede384740579
Stefano <stefano23.riva@mail.polimi.it>
2024-03-19 17:17:22 +0100

    Updated Color.java with colors and associated colors, created json files for the decks

--- src/main/java/model/Color.java
+++ src/main/java/model/Color.java
@@ -1,8 +1,20 @@
 package model;
-
 public enum Color {
     RED,
     GREEN,
     BLUE,
-    PURPLE
+    PURPLE;
+    Color associatedColor;
+    Color(){
+        setAssociatedColor();
+    }
+    private void setAssociatedColor(){
+        RED.associatedColor = GREEN;
+        GREEN.associatedColor = PURPLE;
+        BLUE.associatedColor = RED;
+        PURPLE.associatedColor = BLUE;
+    }
+    public Color getAssociatedColor(Color color){
+        return color.associatedColor;
+    }
 }
