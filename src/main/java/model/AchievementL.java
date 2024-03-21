dbc331ffdc3e041f978d9fc8cd2b157b092f42a1
Stefano <stefano23.riva@mail.polimi.it>
2024-03-21 10:42:09 +0100

    Wrote json input file for GoldCard and ResourceCard

--- src/main/java/model/AchievementL.java
+++ src/main/java/model/AchievementL.java
@@ -6,13 +6,13 @@ import java.util.Set;
 
 public class AchievementL implements Achievement{
     private int basePoint;
-    private String color;
+    private Color color;
     /**
      * Builds cards which achievement is "create a L with two cards of the same color, and one different".
      * They've all the same base points (3)
      * @param color color of 2/3 of the cards
      */
-    public AchievementL(String color){
+    public AchievementL(Color color){
         this.basePoint = 3;
         this.color = color;
     }
