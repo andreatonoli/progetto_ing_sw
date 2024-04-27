f51a71e6868c1a67476351377ce58fb641b0ee1a
Stefano <stefano23.riva@mail.polimi.it>
2024-04-27 19:31:55 +0200

    Added new test case to flip test

--- src/test/java/model/FlipTest.java
+++ src/test/java/model/FlipTest.java
@@ -27,6 +27,7 @@ import java.io.IOException;
 
 
 public class FlipTest {
+
     @Test
     @DisplayName("Flip a card")
     public void FlipCardTest() throws IOException {
@@ -45,6 +46,7 @@ public class FlipTest {
         player.getCardInHand()[0].setCurrentSide();
         assertEquals(a.getColor(), player.getCardInHand()[0].getColor());
         assertNotNull(player.getCardInHand()[0].getSymbols());
+        assertEquals(1, player.getCardInHand()[0].getSymbols().size());
         assertEquals(Symbols.PLANT, player.getCardInHand()[0].getSymbols().getFirst());
         for (CornerEnum co : CornerEnum.values()) {
             assertEquals(Symbols.EMPTY, player.getCardInHand()[0].getCornerSymbol(co));
@@ -74,8 +76,34 @@ public class FlipTest {
     }
 
     @Test
+    @DisplayName("Flip StarterCard")
+    public void FlipStarterCard(){
+        StarterCard s = new StarterCard(new Corner[]{new Corner(Symbols.PLANT), new Corner(Symbols.ANIMAL), new Corner(Symbols.INSECT), new Corner(Symbols.FUNGI)}, 2, new CardBack(new ArrayList<>(List.of(Symbols.FUNGI)), Color.WHITE, new Corner[]{new Corner(Symbols.ANIMAL), new Corner(Symbols.EMPTY), new Corner(Symbols.NOCORNER), new Corner(Symbols.EMPTY)}));
+        //Flip a starter card with only one back symbol
+        s.setCurrentSide();
+        assertEquals(Symbols.ANIMAL,s.getCornerSymbol(CornerEnum.TL));
+        assertEquals(Symbols.EMPTY,s.getCornerSymbol(CornerEnum.TR));
+        assertEquals(Symbols.NOCORNER,s.getCornerSymbol(CornerEnum.BR));
+        assertEquals(Symbols.EMPTY,s.getCornerSymbol(CornerEnum.BL));
+        assertNotNull(s.getSymbols());
+        assertEquals(1, s.getSymbols().size());
+        assertEquals(Symbols.FUNGI, s.getSymbols().getFirst());
+        //Flip a starterCard with more back symbols
+        StarterCard a = new StarterCard(new Corner[]{new Corner(Symbols.PLANT), new Corner(Symbols.INSECT), new Corner(Symbols.FUNGI), new Corner(Symbols.ANIMAL)}, 4, new CardBack(new ArrayList<>(List.of(Symbols.ANIMAL, Symbols.INSECT)), Color.WHITE, new Corner[]{new Corner(Symbols.EMPTY), new Corner(Symbols.EMPTY), new Corner(Symbols.EMPTY), new Corner(Symbols.EMPTY)}));
+        a.setCurrentSide();
+        assertEquals(Symbols.EMPTY, a.getCornerSymbol(CornerEnum.TL));
+        assertEquals(Symbols.EMPTY, a.getCornerSymbol(CornerEnum.TR));
+        assertEquals(Symbols.EMPTY, a.getCornerSymbol(CornerEnum.BR));
+        assertEquals(Symbols.EMPTY, a.getCornerSymbol(CornerEnum.BL));
+        assertNotNull(s.getSymbols());
+        assertEquals(2, a.getSymbols().size());
+        assertEquals(Symbols.ANIMAL, a.getSymbols().getFirst());
+        assertEquals(Symbols.INSECT, a.getSymbols().get(1));
+    }
+
+    @Test
     @DisplayName("Flip StarterCard and Not Valid Placement")
-    public void FlipStarterNotValidPlacement() throws IOException{
+    public void FlipStarterNotValidPlacement() {
         Game game = new Game(4);
         Player player = new Player("pippo", game);
         ResourceCard a = new ResourceCard(new Corner[]{new Corner(Symbols.EMPTY), new Corner(Symbols.EMPTY), new Corner(Symbols.PLANT), new Corner(Symbols.NOCORNER)}, 19, 1);
--- src/test/java/network/RMIServerTest.java
+++ src/test/java/network/RMIServerTest.java
@@ -1,35 +1,36 @@
-//package network;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import Controller.Controller;
-//import model.card.*;
-//import model.enums.Color;
-//import model.enums.CornerEnum;
-//import model.enums.PlayerState;
-//import model.enums.Symbols;
-//import model.exceptions.AlreadyUsedPositionException;
-//import model.exceptions.CostNotSatisfiedException;
-//import model.exceptions.NotInTurnException;
-//import model.exceptions.OccupiedCornerException;
-//import model.player.Player;
-//import network.server.Action;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.DisplayName;
-//import java.io.IOException;
-//public class RMIServerTest {
-//    //TODO: non è un test vero
-//    @Test
-//    @DisplayName("Running test")
-//    public void provaRun(){
-//        spy(), mock(), verify()
-//        Server server = mock(new Server());
-//        Controller co = spy(new Controller(4));
-//        verify(server, )
-//        Action fun = () -> System.out.println("Hello Andrea");
-//        fun.execute();
-//    }
-//}
+package network;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Controller.Controller;
+import model.Game;
+import model.GameBoard;
+import model.card.*;
+import model.enums.Color;
+import model.enums.CornerEnum;
+import model.enums.PlayerState;
+import model.enums.Symbols;
+import model.exceptions.AlreadyUsedPositionException;
+import model.exceptions.CostNotSatisfiedException;
+import model.exceptions.NotInTurnException;
+import model.exceptions.OccupiedCornerException;
+import model.player.Player;
+import network.server.Action;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import java.io.IOException;
+public class RMIServerTest {
+    //TODO: non è un test vero
+    @Test
+    @DisplayName("Running test")
+    public void provaRun(){
+        //spy(), mock(), verify()
+        //Server server = mock(new Server());
+        //Controller co = spy(new Controller(4));
+        //verify(server, )
+        new GameBoard(new Game(4));
+    }
+}
