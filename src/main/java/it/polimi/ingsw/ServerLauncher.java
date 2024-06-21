8d30523037aab4074404a2ca372d3a7962786d16
Stefano <stefano23.riva@mail.polimi.it>
2024-06-21 13:57:10 +0200

    Added ip address argument to server

--- src/main/java/it/polimi/ingsw/ServerLauncher.java
+++ src/main/java/it/polimi/ingsw/ServerLauncher.java
@@ -4,6 +4,11 @@ import it.polimi.ingsw.network.server.Server;
 
 public class ServerLauncher {
     public static void main (String[] args){
+        if (args.length < 1){
+            System.out.println("Insert ip address as argument");
+            return;
+        }
+        System.setProperty("java.rmi.server.hostname", args[0]);
         new Server();
     }
 }
